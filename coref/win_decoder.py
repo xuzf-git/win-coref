297911f98a78f201eb0f2f068c0a0b1d402959f3
xuzf-git <3349297129@qq.com>
2023-01-09 22:23:10 +0800

    fix WinBuilder devce bug

--- coref/win_decoder.py
+++ coref/win_decoder.py
@@ -13,7 +13,6 @@ from coref import utils
 from coref.const import Doc
 from coref.pairwise_encoder import PairwiseEncoder
 
-
 import argparse
 
 
@@ -28,13 +27,13 @@ class WinDecoder(torch.nn.Module):
         """
         super().__init__()
         self.topk = config.top_k
+        self.device = config.device
         self.win_builder = WinBuilder(features, config)
 
     def forward(
-        self,  # type: ignore  # pylint: disable=arguments-differ  #35566 in pytorch
-        mentions: torch.Tensor,
-        doc: Doc
-    ) -> Tuple[torch.Tensor, torch.Tensor]:
+            self,  # type: ignore  # pylint: disable=arguments-differ  #35566 in pytorch
+            mentions: torch.Tensor,
+            doc: Doc) -> Tuple[torch.Tensor, torch.Tensor]:
         """
         Returns top-k antecedent sequence and scores for input mentions
 
@@ -49,16 +48,13 @@ class WinDecoder(torch.nn.Module):
 
         n_mentions = mentions.size(0)
         # generate the local structure and local score
-        global_struct = torch.zeros(self.topk, n_mentions + 1)
-        global_scores = torch.zeros(self.topk, n_mentions + 1)
+        global_struct = torch.zeros(self.topk, n_mentions + 1).to(self.device)
+        global_scores = torch.zeros(self.topk, n_mentions + 1).to(self.device)
         for windows_size in range(1, n_mentions):
             # build local_structure(d + 1) from global_structure(d)
-            # local_struct_origin = global_struct.repeat(2, 1)
-            local_struct = self.win_builder(mentions, doc, global_struct, global_scores, windows_size)
+            local_struct, local_scores = self.win_builder(mentions, doc, global_struct, global_scores, windows_size)
 
-            pass
-
-        # BeamSearch the top-k global structure
+            # BeamSearch the top-k global structure
 
         return
 
@@ -76,9 +72,10 @@ class WinBuilder(torch.nn.Module):
         """
         super().__init__()
         self.batch_size = config.batch_size
+        self.device = config.device
         self.pairwise_encoder = PairwiseEncoder(config)
         self.scorer = AnaphoricityScorer(in_features, config)
-    
+
     @staticmethod
     def _get_features(mention_features: torch.Tensor, pair_features: torch.Tensor):
         """
@@ -94,11 +91,7 @@ class WinBuilder(torch.nn.Module):
         features = torch.cat((a_mentions, b_mentions, similarity, pair_features), dim=1)
         return features
 
-    def forward(self, 
-                mentions: torch.Tensor,
-                doc: Doc,
-                global_struct: torch.Tensor, 
-                global_scores: torch.Tensor,
+    def forward(self, mentions: torch.Tensor, doc: Doc, global_struct: torch.Tensor, global_scores: torch.Tensor,
                 windows_size: int) -> torch.Tensor:
         """
         Args:
@@ -112,35 +105,36 @@ class WinBuilder(torch.nn.Module):
             local_struct: a tensor of shape [k, n_mentions + 1], the local structure
             local_scores: a tensor of shape [k, n_mentions + 1], the local score
         """
-        # build local structure in-batch [batch_size, k, windows_size]
         local_struct_lst: List[torch.Tensor] = []
+        local_scores_lst: List[torch.Tensor] = []
         n_windows = mentions.size(0) - windows_size
+        # build local structure in-batch [batch_size, k, windows_size]
         for i in range(1, n_windows + 1, self.batch_size):
-            global_struct_batch = global_struct[:, i:i + self.batch_size + windows_size]  # [k, batch_size+windows_size]
-            global_scores_batch = global_scores[:, i:i + self.batch_size + windows_size]
+            global_struct_batch = global_struct[:, i:i + self.batch_size + windows_size]  # [k, b_s+w_s]
+            global_scores_batch = global_scores[:, i:i + self.batch_size + windows_size]  # [k, b_s+w_s]
             real_batch_size = global_struct_batch.size(1) - windows_size
-            # To generate a 3D tensor from a 2D tensor by sampling the 2D tensor in a sliding window fashion
-            ## [k, batch_size, windows_size+1]
+            # To generate a 3D tensor from a 2D tensor by sampling in a sliding window fashion [k, b_s, w_s+1]
             local_struct_batch = global_struct_batch.unfold(dimension=1, size=windows_size + 1, step=1)
-            local_scores_batch = global_scores_batch[:, -real_batch_size:]
-            ## new local structure: link the last mention of windows to the first mention, 0 means link to dummy
-            new_link = torch.arange(i, i + real_batch_size).view(1, -1, 1).repeat(local_struct_batch.size(0), 1, 1)
+            local_scores_batch = global_scores_batch[:, -real_batch_size:]  # [k, b_s]
+            # new local structure: link the last mention of windows to the first mention, 0 means link to dummy
+            new_link = torch.arange(i, i + real_batch_size).to(self.device).view(1, -1, 1).repeat(local_struct_batch.size(0), 1, 1)
             local_struct_batch_append = torch.cat((local_struct_batch[:, :, :windows_size], new_link), dim=2)
-            ## score the new local structure
-            new_link_pairs = (torch.arange(i, i + real_batch_size).unsqueeze(1),
-                              torch.arange(i, i + real_batch_size).unsqueeze(1) + windows_size)
+            # score the new local structure
+            new_link_pairs = (torch.arange(i, i + real_batch_size).unsqueeze(1).to(self.device),
+                              torch.arange(i, i + real_batch_size).unsqueeze(1).to(self.device) + windows_size)
             new_link_pairs = torch.cat(new_link_pairs, dim=1) - 1
             pairwise_features = self.pairwise_encoder(new_link_pairs, doc)
             features_batch = self._get_features(mentions[new_link_pairs], pairwise_features)
             new_link_pairs_scores = self.scorer(features_batch)
             local_scores_batch_append = new_link_pairs_scores.unsqueeze(0).repeat(local_scores_batch.size(0), 1)
-            # TODO: think about the local struct's data structure for global beamsearch
-            local_struct_lst.append(local_struct_batch)
-        local_struct = torch.cat(local_struct_lst, dim=1)  # [k, n_windows, windows_size]
-        # add new local structure, which link from the last to the first mention
-        local_struct = local_struct.repeat(1, 1, 2)
+            
+            local_struct_lst.append(torch.cat((local_struct_batch, local_struct_batch_append), dim=0))
+            local_scores_lst.append(torch.cat((local_scores_batch, local_scores_batch_append), dim=0))
+        
+        local_struct = torch.cat(local_struct_lst, dim=1).view(n_windows, -1, windows_size + 1)  # [n_windows, 2k, windows_size + 1]
+        local_scores = torch.cat(local_scores_lst, dim=1).view(n_windows, -1)  # [n_windows, 2k]
 
-        return local_struct
+        return local_struct, local_scores
 
 
 def load_config(config_path: str, section: str) -> Config:
